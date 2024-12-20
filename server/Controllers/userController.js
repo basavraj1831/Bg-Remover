5b68244a41c4051ee8203634f90e1d0e4435c829
basavraj1831 <144144279+basavraj1831@users.noreply.github.com>
2024-12-20 07:20:42 +0530

    Update userController.js

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -89,19 +89,19 @@ const paymentRazorpay = async (req, res) => {
             case 'Basic': 
                 plan = 'Basic'
                 credits = 100
-                amount = 10
+                amount = 100
                 break;
             
             case 'Advanced': 
                 plan = 'Advanced'
                 credits = 500
-                amount = 50
+                amount = 450
                 break;
             
             case 'Business': 
                 plan = 'Business'
                 credits = 5000
-                amount = 250
+                amount = 4000
                 break;
             
             default: {
